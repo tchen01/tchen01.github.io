2e0fa940f96c523b7ad50fabbae101c45e379c9f
Tyler Chen <chentylerl@gmail.com>
2015-11-21 15:54:36 -0500

    k

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,11 @@
 
 var sections = document.getElementsByTagName( 'section' );
+var titles = document.getElementsByClassName( 'about' );
 var container = document.getElementById('contentContainer');
 var whiteSpace = document.getElementById('whitespace');
 
-for(var i=0; i<sections.length; i++){
-    sections[i].addEventListener('mousedown', startTime, false);
-    sections[i].addEventListener('mouseup', sectionClick, false);
+for(var i=0; i<titles.length; i++){
+    titles[i].addEventListener('mouseup', titleClick, false);
 }
 
 
@@ -13,17 +13,11 @@ if( getCookie("tip") !== "false" ){
     tipCreate();
 }
 
-function startTime(){
-    time = new Date();
-}
-function sectionClick(e){
-    if( new Date() - time < 250){
-        var em = this.getElementsByClassName( 'toggle' );
-        for(i=0;i<em.length;i++){
-            em[i].classList.contains('hidden') ? em[i].classList.remove( 'hidden' ) : em[i].classList.add( 'hidden' );
-        } 
-    }
-    
+function titleClick(e){
+    var em = this.parentNode.getElementsByClassName( 'toggle' );
+    for(i=0;i<em.length;i++){
+        em[i].classList.contains('hidden') ? em[i].classList.remove( 'hidden' ) : em[i].classList.add( 'hidden' );
+    } 
 }
 
 function tipCreate(){
@@ -32,12 +26,11 @@ function tipCreate(){
     container.insertBefore(tipSec, sections[1])
     var tipSec = document.getElementById( 'tip' );
     
-    tipSec.outerHTML = "<section class='section' id='tip' ><div class='about'><h3>Click on a card to read more</h3></div><div class='item'><div class='title toggle hidden' onclick='tipRemove()'>remove</div><div class='content toggle hidden' onclick='tipRemove()'>click here to remove this card</div></div><div class='item'><div class='title toggle hidden'>cookies</div><div class='content toggle hidden'>I use one cookie. (apparently it's a trend to announce this)</div></div></section> ";
+    tipSec.outerHTML = "<section class='section' id='tip' ><div class='about'><h3>Click on a card title to read more</h3></div><div class='item'><div class='title toggle hidden' onclick='tipRemove()'>remove</div><div class='content toggle hidden' onclick='tipRemove()'>click here to remove this card</div></div><div class='item'><div class='title toggle hidden'>cookies</div><div class='content toggle hidden'>I use one cookie. (to hide this card in the future)</div></div></section> ";
     
 
     var tipSec = document.getElementById('tip');
-    tipSec.addEventListener('mousedown', startTime, false);
-    tipSec.addEventListener('mouseup', sectionClick, false);
+    tipSec.addEventListener('mouseup', titleClick, false);
 }
 
 function tipRemove(){
